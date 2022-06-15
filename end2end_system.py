b5bfdc7a4c3b14a26c245b6e2d134f5e8cdcccbb
Dung Hoang Tien <htdung167@gmail.com>
2022-06-15 11:14:27 +0700

    add end2end_system

--- end2end_system.py
+++ end2end_system.py
@@ -64,105 +64,74 @@ def get_result_predict(img, cfg_path, weight_path, conf_threshold = 0.5, nms_thr
         result.append([class_ids[i], confidences[i], x, y, x + w, y + h])
 
     end = time.time()
-    print("YOLO Execution time: " + str(end-start))
+    # print("YOLO Execution time: " + str(end-start))
 
     # Trả về topleft và bottomright [class_ids, confidences, x, y, x + w, y + h]
     return result
 
 
 if __name__=="__main__":
-    image = cv2.imread("./test_img_rec/1631.jpg")
+    image = cv2.imread("./test_img/1637.jpg")
+    image_c = image.copy()
+
     cfg_path = "./Fastyolov2/fast-yolov2.cfg"
     weight_path = "./Fastyolov2/fast-yolov2_best.weights"
     with open("./Fastyolov2/yolo.names", 'r') as f:
-        classes = [line.strip() for line in f.readlines()]
+        classes_lp = [line.strip() for line in f.readlines()]
+    
+    cfg_path2 = "./cr_net/crnet.cfg"
+    weight_path2 = "./cr_net/crnet_best.weights"
+    with open("./cr_net/crnet.names", 'r') as f:
+        classes_char = [line.strip() for line in f.readlines()]
 
-    results = get_result_predict(image, cfg_path, weight_path)
-    for result in results:
-        class_id, confidence, tl_x, tl_y, br_x, br_y = result
-        draw_prediction(image, class_id, confidence, round(tl_x), round(tl_y), round(br_x), round(br_y), classes)
-    cv2.imshow("predict", image)
+    pred_bss = get_result_predict(image, cfg_path, weight_path)
+
+    for pred_bs in pred_bss:
+        class_id, confidence, tl_x_bs, tl_y_bs, br_x_bs, br_y_bs = pred_bs
+
+        lp_image = image[round(tl_y_bs):round(br_y_bs), round(tl_x_bs):round(br_x_bs)]
+
+
+        h, w = lp_image.shape[:2]
+        cfg_path2 = "./cr_net/crnet.cfg"
+        weight_path2 = "./cr_net/crnet_best.weights"
+        with open("./cr_net/crnet.names", 'r') as f:
+            classes = [line.strip() for line in f.readlines()]
+
+        pred_chars = get_result_predict(lp_image, cfg_path2, weight_path2, conf_threshold = 0.5, nms_threshold=0.1)
+        first_line = []
+        second_line = []
+        for pred_char in pred_chars:
+            class_id, confidence, tl_x, tl_y, br_x, br_y = pred_char
+            if 0 < (tl_y + br_y) / 2 < h / 2:
+                first_line.append(pred_char)
+            else:
+                second_line.append(pred_char)
+        first_line = sorted(first_line, key=lambda x:x[2])
+        second_line = sorted(second_line, key=lambda x:x[2])
+        result = "".join([*[classes[x[0]] for x in first_line], *[classes[y[0]] for y in second_line]])
+        print(result)
+
+        cv2.rectangle(image_c, (round(tl_x_bs), round(tl_y_bs)), (round(br_x_bs), round(br_y_bs)), (0,0,255), 2)
+        cv2.putText(image_c, result, (round(tl_x_bs) - 10, round(tl_y_bs) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255), 2)
+
+    #     # draw_prediction(image_c, class_id, confidence, round(tl_x), round(tl_y), round(br_x), round(br_y), classes)
+    cv2.imshow("predict", image_c)
     cv2.waitKey(0)
 
 
 
 
-# # Width = image.shape[1]
-# # Height = image.shape[0]
-# # scale = 1/255.0
 
-# # classes = None
+    
 
-# # with open("./Fastyolov2/yolo.names", 'r') as f:
-# #     classes = [line.strip() for line in f.readlines()]
+    # cv2.imshow("predict", image)
+    # cv2.waitKey(0)
 
 
 
-# cfg_path = "./Fastyolov2/fast-yolov2.cfg"
-# weight_path = "./Fastyolov2/fast-yolov2_best.weights"
-
-# # net = cv2.dnn.readNet(cfg_path, weight_path)
-# # net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-# # blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
-
-# # net.setInput(blob)
-# # outs = net.forward(get_output_layers(net))
-# # print(outs[1])
-
-# class_ids = []
-# confidences = []
-# boxes = []
-# conf_threshold = 0.5
-# nms_threshold = 0.4
-
-# start = time.time()
-
-# for out in outs:
-#     # print(out.shape)
-#     for detection in out:
-#         scores = detection[5:]
-#         # print(scores)
-#         class_id = np.argmax(scores)
-#         # print(scores)
-#         confidence = scores[class_id]
-#         # print(confidence)
-#         if confidence > 0.5:
-#             # print(confidence)
-#             center_x = int(detection[0] * Width)
-#             center_y = int(detection[1] * Height)
-#             w = int(detection[2] * Width)
-#             h = int(detection[3] * Height)
-#             x = center_x - w / 2
-#             y = center_y - h / 2
-#             class_ids.append(class_id)
-#             confidences.append(float(confidence))
-#             boxes.append([x, y, w, h])
-# # print(confidences)
-# indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
-
-# # print(indices)
-# # print(boxes)
-# # print(confidences)
-# # for i in indices:
-# #     # print(confidence)
-# #     box = boxes[i]
-# #     x = box[0]
-# #     y = box[1]
-# #     w = box[2]
-# #     h = box[3]
-# #     draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h))
-
-# cv2.imshow("predict", image)
 
 
-# end = time.time()
-# print("YOLO Execution time: " + str(end-start))
-
-
-# cv2.waitKey()
-
-# # cv2.imwrite("object-detection.jpg", image)
-# cv2.destroyAllWindows()
 
 
 
--- recognize_LP_darknet.py
+++ recognize_LP_darknet.py
@@ -71,98 +71,37 @@ def get_result_predict(img, cfg_path, weight_path, conf_threshold = 0.5, nms_thr
 
 
 if __name__=="__main__":
-    image = cv2.imread("./test_img_rec/1631.jpg")
-    cfg_path = "./Fastyolov2/fast-yolov2.cfg"
-    weight_path = "./Fastyolov2/fast-yolov2_best.weights"
-    with open("./Fastyolov2/yolo.names", 'r') as f:
+    image = cv2.imread("./test_img_rec/24.jpg")
+    h, w = image.shape[:2]
+    image = cv2.resize(image, (352, 128))
+
+    h_new, w_new = image.shape[:2]
+
+    cfg_path = "./cr_net/crnet.cfg"
+    weight_path = "./cr_net/crnet_best.weights"
+    with open("./cr_net/crnet.names", 'r') as f:
         classes = [line.strip() for line in f.readlines()]
 
-    results = get_result_predict(image, cfg_path, weight_path)
+    results = get_result_predict(image, cfg_path, weight_path, conf_threshold = 0.5, nms_threshold=0.1)
+    first_line = []
+    second_line = []
     for result in results:
         class_id, confidence, tl_x, tl_y, br_x, br_y = result
+        # print(result)
+        if 0 < (tl_y + br_y) / 2 < h_new / 2:
+            first_line.append(result)
+        else:
+            second_line.append(result)
+        
         draw_prediction(image, class_id, confidence, round(tl_x), round(tl_y), round(br_x), round(br_y), classes)
+
+    first_line = sorted(first_line, key=lambda x:x[2])
+    second_line = sorted(second_line, key=lambda x:x[2])
+    result = "".join([*[classes[x[0]] for x in first_line], *[classes[y[0]] for y in second_line]])
+    
+    print(result)
+
+    image = cv2.resize(image, (h*3, w*3))
     cv2.imshow("predict", image)
     cv2.waitKey(0)
-
-
-
-
-# # Width = image.shape[1]
-# # Height = image.shape[0]
-# # scale = 1/255.0
-
-# # classes = None
-
-# # with open("./Fastyolov2/yolo.names", 'r') as f:
-# #     classes = [line.strip() for line in f.readlines()]
-
-
-
-# cfg_path = "./Fastyolov2/fast-yolov2.cfg"
-# weight_path = "./Fastyolov2/fast-yolov2_best.weights"
-
-# # net = cv2.dnn.readNet(cfg_path, weight_path)
-# # net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-# # blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
-
-# # net.setInput(blob)
-# # outs = net.forward(get_output_layers(net))
-# # print(outs[1])
-
-# class_ids = []
-# confidences = []
-# boxes = []
-# conf_threshold = 0.5
-# nms_threshold = 0.4
-
-# start = time.time()
-
-# for out in outs:
-#     # print(out.shape)
-#     for detection in out:
-#         scores = detection[5:]
-#         # print(scores)
-#         class_id = np.argmax(scores)
-#         # print(scores)
-#         confidence = scores[class_id]
-#         # print(confidence)
-#         if confidence > 0.5:
-#             # print(confidence)
-#             center_x = int(detection[0] * Width)
-#             center_y = int(detection[1] * Height)
-#             w = int(detection[2] * Width)
-#             h = int(detection[3] * Height)
-#             x = center_x - w / 2
-#             y = center_y - h / 2
-#             class_ids.append(class_id)
-#             confidences.append(float(confidence))
-#             boxes.append([x, y, w, h])
-# # print(confidences)
-# indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
-
-# # print(indices)
-# # print(boxes)
-# # print(confidences)
-# # for i in indices:
-# #     # print(confidence)
-# #     box = boxes[i]
-# #     x = box[0]
-# #     y = box[1]
-# #     w = box[2]
-# #     h = box[3]
-# #     draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h))
-
-# cv2.imshow("predict", image)
-
-
-# end = time.time()
-# print("YOLO Execution time: " + str(end-start))
-
-
-# cv2.waitKey()
-
-# # cv2.imwrite("object-detection.jpg", image)
-# cv2.destroyAllWindows()
-
-
-
+    
